17f70e8f2bafeebe78fba89ba1c79ec72d203cd9
Tom Schroeder <j.tom.schroeder@gmail.com>
2016-08-07 13:44:51 -0500

    Show char.

--- test/test_stream.cpp
+++ test/test_stream.cpp
@@ -8,6 +8,6 @@ using namespace lambda::streams;
 TEST(lambda, stream) {
     auto s = stream($::vector(1, 2, 3, 4)) | map(plus(1)) | filter(less(4));
     while (auto val = s.next()) {
-        printer << val << "\n";
+        printer << val << '\n';
     }
 }
