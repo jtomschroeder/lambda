7c0559b293163237bc1eb8a1620637b092d21e86
Tom Schroeder <j.tom.schroeder@gmail.com>
2016-08-11 13:13:44 -0500

    Add benchmarks! ✔︎

---
+++ bench/bench_lambda.cpp
@@ -0,0 +1,17 @@
+
+#include "benchmark/benchmark.h"
+#include "lambda.hpp"
+
+using namespace lambda;
+using namespace lambda::streams;
+
+class LambdaPerf : public benchmark::Fixture {};
+
+BENCHMARK_F(LambdaPerf, Streams)(benchmark::State &state) {
+    while (state.KeepRunning()) {
+        let solution = ints(0, 1000) | filter(multipleOf(3) || multipleOf(5)) | sum;
+        benchmark::DoNotOptimize(solution);
+    }
+}
+
+BENCHMARK_MAIN();
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p build && cd build
 
 if [ ! -f Makefile ]; then
-  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 fi
 
 make $@
